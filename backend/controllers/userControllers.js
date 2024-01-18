92233cb490b99ea3be5a261cce3924a1c9b56b14
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-01-18 00:02:04 -0300

    Backend auth/login api comparing encpryted passwords

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -28,8 +28,27 @@ const registerUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(400);
-    throw new Error("User not found");
+    throw new Error("User not created");
   }
 });
 
-export { registerUser };
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+export { registerUser, authUser };
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import databaseConnection from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
-import {
-  errorHandler,
-  notFound,
-} from "./middlewares/errorMiddlewareErrorHandler.js";
 
 //Server Configs
 const app = express();
@@ -17,8 +13,6 @@ databaseConnection();
 
 //Routes()
 app.use("/users", userRouter);
-// app.use(notFound);
-// app.use(errorHandler);
 
 //Server Port
 const PORT = process.env.PORT || 5000;
